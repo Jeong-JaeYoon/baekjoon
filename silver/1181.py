60bf61e8decd773084b928cd3d5510823903b023
Jeong-JaeYoon <68625698+Jeong-JaeYoon@users.noreply.github.com>
2026-01-28 23:17:45 +0900

    1181 해결

---
+++ silver/1181.py
@@ -0,0 +1,11 @@
+n = int(input())
+word_list = []
+
+for _ in range(n):
+    word_list.append(str(input()))
+
+word_list = list(set(word_list))
+word_list.sort(key=lambda x: (len(x), x))
+
+for word in word_list:
+    print(word)
